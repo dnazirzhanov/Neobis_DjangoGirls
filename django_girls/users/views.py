d7b68aa0c9c2e12308c695e58738b2b66002345e
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-01-22 14:58:52 +0600

    create a User Registration functionality

--- django_girls/users/views.py
+++ django_girls/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistrationForm
+from django.contrib import messages
 
-# Create your views here.
+def user_register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/user_register.html', {'form': form})
