33423b02987d821e2c593b1288f586fa4966b773
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-01-22 15:16:32 +0600

    create: add post, edit post, delete post and post list functionality

--- django_girls/blog/views.py
+++ django_girls/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
+
+@login_required
+def update_post(request, pk):
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/update_post.html', {'form': form, 'post': post})
+
+@login_required
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog:post_list')
+    return render(request, 'blog/delete_post.html', {'post': post})
